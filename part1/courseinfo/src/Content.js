56f4ef14a87c93d5807fcf278bc3cf1f466151d7
adi-pshr <adi.pshr@gmail.com>
2022-07-17 13:59:57 +0530

    complete exercise 1.3

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -7,6 +7,9 @@ export default function Content(props) {
       <Part part={props.part1} exercises={props.exercises1}/>
       <Part part={props.part2} exercises={props.exercises2}/>
       <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
   );
 }

bf1ef461a35b8e556d55f9d2fdace0e994ad18f9
adi-pshr <adi.pshr@gmail.com>
2022-07-17 19:08:07 +0530

    complete exercise 1.5

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -4,9 +4,18 @@ import Part from "./Part";
 export default function Content(props) {
   return (
     <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part
+        part={props.course.parts[0].name}
+        exercises={props.course.parts[0].exercises}
+      />
+      <Part
+        part={props.course.parts[1].name}
+        exercises={props.course.parts[1].exercises}
+      />
+      <Part
+        part={props.course.parts[2].name}
+        exercises={props.course.parts[2].exercises}
+      />
     </div>
   );
 }

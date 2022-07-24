38b1f25195cec2ae4f350d6af72d112147980991
adi-pshr <adi.pshr@gmail.com>
2022-07-24 15:45:57 +0530

    complete exercise 1.11

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -6,13 +6,15 @@ export default function Statistics({ stats }) {
     return <p>No Feedback Given</p>;
   }
   return (
-    <div>
-      <StatisticLine text={"Good"} value={stats.goodRating} />
-      <StatisticLine text={"Neutral"} value={stats.neutralRating} />
-      <StatisticLine text={"Bad"} value={stats.badRating} />
-      <StatisticLine text={"All"} value={stats.totalRating} />
-      <StatisticLine text={"Average"} value={stats.averageRating} />
-      <StatisticLine text={"Percentage"} value={`${stats.positivePer} %`} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text={"Good"} value={stats.goodRating} />
+        <StatisticLine text={"Neutral"} value={stats.neutralRating} />
+        <StatisticLine text={"Bad"} value={stats.badRating} />
+        <StatisticLine text={"All"} value={stats.totalRating} />
+        <StatisticLine text={"Average"} value={stats.averageRating} />
+        <StatisticLine text={"Percentage"} value={`${stats.positivePer} %`} />
+      </tbody>
+    </table>
   );
 }
